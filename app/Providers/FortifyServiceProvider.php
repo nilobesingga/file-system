2f762af247474771da10777e178764b5e2c468ab
Nilo Besingga <nilo.besingga@cresco.org>
2025-03-26 17:46:40 +0400

    update

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -1,10 +1,14 @@
 <?php
 namespace App\Providers;
 
+use App\Http\Controllers\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Fortify;
 use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
 use Illuminate\Support\ServiceProvider;
@@ -13,10 +17,16 @@ class FortifyServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        Fortify::verifyEmailView(function () {
+            return view('auth.verify-email');
+        });
+        Fortify::confirmPasswordView(function () {
+            return view('auth.confirm-password');
+        });
         Fortify::loginView(function () {
             return view('auth.login');
         });
-
+        $this->configureRoutes();
         // Override login response
         $this->app->singleton(LoginResponseContract::class, function () {
             return new class implements LoginResponseContract {
@@ -37,6 +47,31 @@ class FortifyServiceProvider extends ServiceProvider
         // Rate limiter for 2FA
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->user()->id ?? $request->ip());
+        });
+    }
+
+    protected function configureRoutes()
+    {
+        Route::group([
+            'middleware' => ['web'],
+            'as' => 'password.',
+        ], function () {
+            Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])
+                ->middleware('auth')
+                ->name('confirm');
+        });
+
+        Route::group([
+            'middleware' => ['web'],
+            'as' => 'verification.',
+        ], function () {
+            Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])
+                ->middleware('auth')
+                ->name('notice');
+
+            Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
+                ->middleware(['auth', 'signed', 'throttle:6,1'])
+                ->name('verify');
         });
     }
 }
