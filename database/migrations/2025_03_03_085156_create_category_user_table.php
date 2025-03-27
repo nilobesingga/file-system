316ea844e397ee83a6eb17a1d384685d66651077
Nilo Besingga <nilo.besingga@cresco.org>
2025-03-27 09:18:04 +0400

    update

--- database/migrations/2025_03_03_085156_create_category_user_table.php
+++ database/migrations/2025_03_03_085156_create_category_user_table.php
@@ -11,12 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('category_user', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('category_id')->constrained()->onDelete('cascade');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('category_user')) {
+            Schema::create('category_user', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('user_id')->constrained()->onDelete('cascade');
+                $table->foreignId('category_id')->constrained()->onDelete('cascade');
+                $table->timestamps();
+            });
+        }
     }
 
     /**
