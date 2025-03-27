e5ab8c533c4e3420282807d02b66ae3fe84b9abc
Nilo Besingga <nilo.besingga@cresco.org>
2025-03-27 10:45:26 +0400

    update

--- database/migrations/2025_03_10_045309_add_two_factor_columns_to_users_table.php
+++ database/migrations/2025_03_10_045309_add_two_factor_columns_to_users_table.php
@@ -33,12 +33,14 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn([
-                'two_factor_secret',
-                'two_factor_recovery_codes',
-                'two_factor_confirmed_at',
-            ]);
-        });
+        if (!Schema::hasColumn('users', 'two_factor_secret')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn([
+                    'two_factor_secret',
+                    'two_factor_recovery_codes',
+                    'two_factor_confirmed_at',
+                ]);
+            });
+        }
     }
 };
